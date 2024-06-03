525ab0b82551c14ca64cddbb82ff0dff0f70caa8
Ashish1965 <121724473+Ashish1965@users.noreply.github.com>
2024-06-03 20:09:46 +0530

    Update orders.js

--- pages/api/orders.js
+++ pages/api/orders.js
@@ -5,7 +5,21 @@ import initDB from "@/helpers/initDB";
 
 initDB()
 
-export default Authenticated (async(req , res) => {
-    const orders = await Order.find({user : req.userId}).populate("products.product")
-    res.status(200).json(orders)
-})
+export default Authenticated(async (req, res) => {
+  const orders = await Order.find({ user: req.userId }).populate(
+    "products.product"
+  );
+  const result = orders.map((order)=>{
+    return order.products.filter((ord) =>{
+      return ord.product !== null;
+    })
+  })
+  console.log(result);
+  
+  let i = 0;
+  orders.map((order)=>{
+    order.products = result[i++];
+  })
+  console.log(orders);
+  res.status(200).json(orders);
+});
