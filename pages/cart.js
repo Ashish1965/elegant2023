61847ef75a3b9f76a0ee95366842269dcf899262
Ashish1965 <121724473+Ashish1965@users.noreply.github.com>
2024-06-03 17:50:49 +0530

    Update cart.js

--- pages/cart.js
+++ pages/cart.js
@@ -140,7 +140,6 @@ const Cart = (props) => {
           <div className="flex max-w-screen-2xl items-center justify-center mt-7">
               <button onClick={()=>{
                 handleCheckout();
-                handleReload();
               }} className="flex hover:bg-blue-500 active:scale-95 bg-blue-600 px-10 py-3 rounded-xl text-white font-serif text-xl space-x-3">
                 <p>checkout</p>
                 <p>₹{Price}</p>
